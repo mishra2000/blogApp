ae342555b38b455f32ee8903524e7d210c57dbaa
Utkarsh Mishra <utkarshsrmcem111@gmail.com>
2021-11-12 23:53:57 +0530

    added devise, everything working fine, added current user functionality for edit update destroy of blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!,except: [:index,:show]
+  before_action :correct_user, only: [:edit,:update,:destroy]
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    # @article = Article.new
+    @article = current_user.articles.build
+  end
+  def create
+    # @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  def correct_user
+    @article = current_user.articles.find_by(id: params[:id])
+    redirect_to articles_path,notice: "not authorised" if @article.nil?
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title,:body,:user_id)
+    end
+end
